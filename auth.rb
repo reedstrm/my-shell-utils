ca7401aa418601662c646dde9ecf9ed2e52232c5
Ross Reedstrom <reedstrm@gmail.com>
2019-02-20 12:33:23 -0600

    rubocop fixups

--- auth.rb
+++ auth.rb
@@ -1,53 +1,49 @@
 #!/usr/bin/ruby
-require "rotp"
-require "quirc"
-require "oily_png"
-require "pathname"
+require 'rotp'
+require 'quirc'
+require 'oily_png'
+require 'pathname'
 
-require "clipboard"
+require 'clipboard'
 
 def totps(imgs)
-    imgs.map {
-        |img_path|
-        img = ChunkyPNG::Image.from_file(img_path)
-        params = CGI::parse URI.parse(Quirc.decode(img).first.payload).query
-        ROTP::TOTP.new(params['secret'][0],issuer: params['issuer'][0])
-    }
+  imgs.map do |img_path|
+    img = ChunkyPNG::Image.from_file(img_path)
+    params = CGI.parse URI.parse(Quirc.decode(img).first.payload).query
+    ROTP::TOTP.new(params['secret'][0], issuer: params['issuer'][0])
+  end
 end
 
 begin
-if ARGV.length == 0
-    puts "Need at least one QR image file"
-    exit 1 
-end
+  if ARGV.empty?
+    puts 'Need at least one QR image file'
+    exit 1
+  end
 
-if File::directory? ARGV[0]
-        imgs = Pathname.new(ARGV[0]).children.map(&:to_path) 
-else
+  if File.directory? ARGV[0]
+    imgs = Pathname.new(ARGV[0]).children.map(&:to_path)
+  else
     imgs = ARGV
-end
+  end
 
-totps = totps imgs
+  totps = totps imgs
 
-stop_time = Time.now + 30
+  stop_time = Time.now + 30
 
-puts "\n" * totps.length
+  puts "\n" * totps.length
 
-begin
+  begin
     while Time.now < stop_time
-        print "\033[F" * totps.length
-        totps.each {
-            |totp|
-            code = totp.now()
-            puts code + "\t" + totp.issuer
-            if totps.length == 1
-                Clipboard.copy code
-            end
-            }
-        sleep 1
-        end
-rescue Interrupt => e
+      print "\033[F" * totps.length
+      totps.each do |totp|
+        code = totp.now
+        puts code + "\t" + totp.issuer
+        Clipboard.copy code if totps.length == 1
+      end
+      sleep 1
+    end
+  rescue Interrupt
     puts "\r"
     exit
-end
+  end
 end
